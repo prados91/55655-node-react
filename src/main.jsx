6c460e99ba725559f716818d1613d5241123bee6
Augusto <augustoprados91@gmail.com>
2024-04-27 18:06:38 -0300

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,4 @@
 import ReactDOM from 'react-dom/client'
-import React from 'react'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { UserProvider } from './context/UserContext.jsx';
