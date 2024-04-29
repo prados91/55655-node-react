ab12a98e90e287ade136cec94ad90c5ecbaea5a6
Augusto <augustoprados91@gmail.com>
2024-04-29 14:11:28 -0300

    Contenedor de restore

---
+++ src/components/RestoreContainer/RestoreContainer.jsx
@@ -0,0 +1,62 @@
+import Loading from "../Loading/Loading"
+import RestoreInfo from "../RestoreInfo/RestoreInfo"
+import { useEffect, useState } from "react"
+import axios from "axios"
+import Swal from "sweetalert2"
+
+const RestoreContainer = () => {
+
+
+    const [exist, setExist] = useState(false)
+    const [uid, setUid] = useState("")
+    const urlParams = new URLSearchParams(window.location.search);
+    const token = urlParams.get('token');
+    const LINK_TOKEN = `http://localhost:8080/api/users/${token}`
+    axios.defaults.withCredentials = true;
+
+
+    const functionVerifiy = async () => {
+        try {
+            const res = await axios.post(LINK_TOKEN);
+            console.log(res)
+            if (res.data.statusCode === 200) {
+                setUid(res.data.user_id)
+                console.log("OK")
+                setExist(true)
+            }
+            else {
+                Swal.fire({
+                    title: "LINK EXPIRED",//res.data.message,
+                    icon: "error",
+                    confirmButtonColor: "#3085d6",
+                    confirmButtonText: "OK",
+                }).then((result) => {
+                    if (result.isConfirmed || !result.isConfirmed) {
+                        location.replace("/")
+                    }
+                })
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        if (token) {
+            setExist(false)
+            functionVerifiy()
+        }
+    }, [])
+
+    console.log(exist)
+
+    return (
+        <>
+            {!exist ?
+                (<Loading />) :
+                (<RestoreInfo uid={uid} />)}
+        </>
+    )
+}
+
+export default RestoreContainer
