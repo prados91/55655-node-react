598067cd8cbb8043715758388205ae0ff5693388
Augusto <augustoprados91@gmail.com>
2024-05-29 20:34:57 -0300

    cart item

--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -1,25 +1,22 @@
-import React from 'react'
-import { useContext } from "react"
-import { CartContext } from '../../context/CartContext'
+
 
 import './CartItem.css'
 
-const CartItem = () => {
-    const { cart, deleteItemFromCart } = useContext(CartContext);
+const CartItem = ({ cart, deleteOrder }) => {
     return (
         <div>
             {cart.map((p) => (
                 <div className="cartItem__producto" key={p.id}>
                     <div className="cartItem__producto--detail">
-                        <button className="cartItem__producto--btnDelete" onClick={() => deleteItemFromCart(p.id)}>
+                        <button className="cartItem__producto--btnDelete" onClick={() => deleteOrder(p._id)}>
                             X
                         </button>
-                        <img src={p.photo} alt={p.title} />
+                        <img src={p.product_id.photo} alt={p.title} />
                         <div>
-                            {p.title} (x {p.quantity}u.)
+                            {p.product_id.title} (x {p.quantity}u.)
                         </div>
                     </div>
-                    <div className="cartItem__producto--price">${p.quantity * p.price}</div>
+                    <div className="cartItem__producto--price">USD {p.quantity * p.product_id.price}</div>
                 </div>
             ))}
 
