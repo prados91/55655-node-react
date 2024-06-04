d178f11865a88214024b05c6df08e426cdb3ede5
Augusto <augustoprados91@gmail.com>
2024-06-04 19:03:34 -0300

    fixes

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,6 +1,7 @@
 import { createContext } from "react";
 import PropTypes from "prop-types";
 import axios from "axios";
+import Swal from "sweetalert2";
 
 export const UserContext = createContext([]);
 
@@ -8,10 +9,21 @@ export const UserProvider = ({ children }) => {
 
     const API_USER = "http://localhost:8080/api/sessions/me"
     const verifyUser = async () => {
-        let cookie = document.cookie.split("; ")
-        cookie = cookie.find(each => each.split("=")[0] === "token")
-        const res = await axios.post(API_USER, cookie)
-        return res.data.response
+        try {
+            let cookie = document.cookie.split("; ")
+            cookie = cookie.find(each => each.split("=")[0] === "token")
+            const res = await axios.post(API_USER, cookie)
+            return res.data.response
+        } catch (error) {
+            Swal.fire({
+                title: `${error.message}`,
+                icon: "error",
+                text: "Please, try again in a while.",
+            }).then(() => {
+                location.replace('/');
+            });
+        }
+
     }
     return (
         <UserContext.Provider value={{ verifyUser }}>
